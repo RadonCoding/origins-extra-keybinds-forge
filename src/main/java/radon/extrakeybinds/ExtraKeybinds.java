cb2a322133ce33bef5966d023d57cfd1d0056d09
RadonCoding <henry.welch@reborn.com>
2026-05-20 06:39:55 +0300

    Initial commit

--- src/main/java/radon/extrakeybinds/ExtraKeybinds.java
+++ src/main/java/radon/extrakeybinds/ExtraKeybinds.java
@@ -4,9 +4,10 @@ import com.mojang.blaze3d.platform.InputConstants;
 import io.github.apace100.apoli.ApoliClient;
 import io.github.apace100.origins.Origins;
 import net.minecraft.client.KeyMapping;
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.client.ClientRegistry;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.lwjgl.glfw.GLFW;
 
@@ -23,12 +24,12 @@ public class ExtraKeybinds {
             "denary",
     };
 
-    public ExtraKeybinds(FMLJavaModLoadingContext context) {
-        IEventBus modEventBus = context.getModEventBus();
-        modEventBus.addListener(this::onRegisterKeyMappings);
+    public ExtraKeybinds() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::clientSetup);
     }
 
-    private void onRegisterKeyMappings(final RegisterKeyMappingsEvent event) {
+    private void clientSetup(final FMLClientSetupEvent event) {
         for (String key : KEYS) {
             KeyMapping mapping = new KeyMapping(
                     String.format("key.origins.%s_active", key),
@@ -38,7 +39,7 @@ public class ExtraKeybinds {
             );
             ApoliClient.registerPowerKeybinding(mapping.getName(), mapping);
             ApoliClient.registerPowerKeybinding(key, mapping);
-            event.register(mapping);
+            ClientRegistry.registerKeyBinding(mapping);
         }
     }
 }
